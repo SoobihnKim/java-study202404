f6716c187b6e89d0f4b6aa101eec964b3aec7287
soo <ksbsj92@naver.com>
2024-04-08 13:57:07 +0900

    # 배열 퀴즈

--- src/day02/ArrayQuiz01.java
+++ src/day02/ArrayQuiz01.java
@@ -1,9 +1,37 @@
 package day02;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ArrayQuiz01 {
 
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
 
+        System.out.println("# 먹고 싶은 음식을 입력하세요!");
+        System.out.println("# 입력을 중지하려면 <그만>이라고 입력하세요.");
+
+        String[] foodList = {};
+
+        while (true) {
+            System.out.print(">> ");
+            String newFood = scan.nextLine();
+
+            if (newFood.equals("그만")) break;
+            else {
+                int[] temp = new int[foodList.length + 1];
+                for (int i = 0; i < foodList.length; i++) {
+                    temp[i] = Integer.parseInt(foodList[i]);
+                }
+//                temp[temp.length - 1] = newFood;
+
+                // 4. numbers에 저장된 주소 값을 temp의 주소 값으로 변경
+//                foodList = temp;
+
+                temp = null;
+            }
+            System.out.printf("먹고 싶은 음식리스트: %s", Arrays.toString(foodList));
+        }
     }
 }
---
+++ src/day02/ArrayQuiz02.java
@@ -0,0 +1,18 @@
+package day02;
+
+import java.util.Scanner;
+
+public class ArrayQuiz02 {
+
+    public static void main(String[] args) {
+
+        Scanner sc =  new Scanner(System.in);
+
+        System.out.print("* 우리반 학생들의 별명: [영웅재중, 최강창민, 시아준수, 믹키유천, 유노윤호]");
+        System.out.print(" - 삭제할 학생의 별명을 입력하세요!");
+        while (true) {
+
+        }
+
+    }
+}
