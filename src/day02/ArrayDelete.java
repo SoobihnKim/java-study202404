0a734f12958d5726e8a430390f79d4b33c84b9d1
soo <ksbsj92@naver.com>
2024-04-08 12:03:46 +0900

    # 배열 인덱스 삭제

--- src/day02/ArrayDelete.java
+++ src/day02/ArrayDelete.java
@@ -1,13 +1,34 @@
 package day02;
 
+import java.util.Arrays;
+
 public class ArrayDelete {
 
     public static void main(String[] args) {
 
+        // 앞으로 당기고 pop 마지막 삭제
         // 배열에서 원하는 위치의 값 삭제하기
         int[] numbers = {1, 3, 5, 7, 9, 11};
 
         int targetIndex = 2;
+
+        for (int i = targetIndex; i < numbers.length - 1; i++) {
+            numbers[i] = numbers[i + 1];
+        }
+
+//        numbers[2] = numbers[3]; //[1, 3, 7, 7, 9, 11]
+//        numbers[3] = numbers[4]; //[1, 3, 7, 9, 9, 11]
+//        numbers[4] = numbers[5]; //[1, 3, 7, 9, 11, 11]
+
+        int[] temp = new int[numbers.length - 1];
+        for (int i = 0; i < temp.length; i++) {
+            temp[i] = numbers[i];
+        }
+
+        numbers = temp;
+        temp = null;
+
+        System.out.println(Arrays.toString(numbers));
 
 
     }
