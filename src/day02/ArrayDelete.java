749e6af8920a4fc690a8d82bf38e4be241e2f0b3
soo <ksbsj92@naver.com>
2024-04-08 11:52:11 +0900

    # 배열복사, push, pop

---
+++ src/day02/ArrayDelete.java
@@ -0,0 +1,14 @@
+package day02;
+
+public class ArrayDelete {
+
+    public static void main(String[] args) {
+
+        // 배열에서 원하는 위치의 값 삭제하기
+        int[] numbers = {1, 3, 5, 7, 9, 11};
+
+        int targetIndex = 2;
+
+
+    }
+}
