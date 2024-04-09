a28c8768e6ba85802687c18450907f02e89b3c53
soo <ksbsj92@naver.com>
2024-04-09 11:01:17 +0900

    # 2차원 배열 Matrix

--- src/day03/Matrix.java
+++ src/day03/Matrix.java
@@ -34,5 +34,46 @@ public class Matrix {
         System.out.println(Arrays.deepToString(classScores));
         //[[77, 100, 99], [100, 22, 56], [40, 90, 80]]
 
+        System.out.println("=========================");
+
+        System.out.println("2차원배열의 2번 인덱스 주소:  " + classScores[2]); // [I@2133c8f8
+        System.out.println("scoresHong 주소:  " + scoresHong); // [I@2133c8f8
+
+        // 2차원 배열의 순회
+        for (int i = 0; i < classScores.length; i++) {
+            for (int j = 0; j < classScores[i].length; j++) {
+                System.out.printf("%d", classScores[i][j]);
+            }
+            System.out.println();
+        }
+
+        // js: for ~ of, java: enhanced for loop, iteration, foreach
+        System.out.println("===========================");
+
+        for (int[] scores : classScores) {
+            for(int n : scores) {
+                System.out.printf("%d ", n);
+            }
+            System.out.println(); // \n
+        }
+
+        System.out.println("=========================");
+
+        int[][] arr2by3 = {
+                {10, 20, 30},
+                {40, 50, 60}
+        };
+
+        int[][] arr4by5 = new  int[4][5];
+        for (int[] ints : arr4by5) {
+            for (int anInt : ints) {
+                System.out.printf("%d ", anInt);
+            }
+            System.out.println();
+        }
+
+
+
+
     }
 }
