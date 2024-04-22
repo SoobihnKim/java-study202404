e6e5f5773c12d7a2bd660472dbe4bce242e2e1de
soo <ksbsj92@naver.com>
2024-04-22 11:20:00 +0900

    # 인터페이스

--- src/day10/inter/animal/Cat.java
+++ src/day10/inter/animal/Cat.java
@@ -1,10 +1,14 @@
 package day10.inter.animal;
 
-public class Rabbit extends Animal implements Pet, Wild {
+public class Cat extends Animal implements Pet, Aggressive{
+    @Override
+    public void hunt(Pet pet) {
+
+    }
 
     @Override
     public void handle() {
-        this.age = 10;
+
     }
 
     @Override
@@ -16,10 +20,4 @@ public class Rabbit extends Animal implements Pet, Wild {
     public void violent() {
 
     }
-
-    // 사냥 기능 없고
-
-    // 난폭한 성격
-
-    // 애완동물로 키우기 가능
 }
